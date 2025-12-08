c7dfeb0b8334fa035e2ceb916035760cfc506168
Folomeyev Andrey <andreyfoma092@gmail.com>
2025-12-08 20:12:27 +0300

    дз

---
+++ 2025.10.06 Homework-2/task 3/task 3.cpp
@@ -0,0 +1,20 @@
+﻿#include<cstdio>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	scanf_s("%d", &n);
+	if (n == 1)
+	{
+		printf("0\n");
+	}
+	else if (n % 2 == 0)
+	{
+	 printf("%d\n", n / 2);
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
+	return 0;
+}
