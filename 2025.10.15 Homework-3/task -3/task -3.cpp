3b924979fde3949d0caf351a7c47f132bf0f4ebe
Folomeyev Andrey <andreyfoma092@gmail.com>
2025-12-08 20:07:59 +0300

    дз

---
+++ 2025.10.15 Homework-3/task -3/task -3.cpp
@@ -0,0 +1,24 @@
+﻿#include<cstdio>
+
+int main(int argc, char** argv)
+{
+    int a = 0;
+    int b = 0;
+    scanf_s("%d %d", &a, &b);
+    int x = 0;
+    x = a;
+    int y = 0;
+    y = b;
+    int d = 0;
+    while (y != 0)
+    {
+        d = y;
+        y = x % y;
+        x = d;
+    }
+    int g = x;
+    long long l = 0;
+    l = (long long)a / g * b;
+    printf("%lld", l);
+    return 0;
+}
---
+++ 2025.10.15 Homework-3/task-1/task-1.cpp
@@ -0,0 +1,30 @@
+﻿#include<cstdio>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+    scanf_s("%d", &n);
+
+    if (n == 0) 
+    {
+    printf("0");
+    }
+    else if (n == 1)
+    {
+     printf("1");
+    }
+    else 
+    {
+        int a = 0, b = 1;
+        int i = 2;
+        while (i <= n)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+            i++;
+        }
+        printf("%d", b);
+    }
+	return 0;
+}
