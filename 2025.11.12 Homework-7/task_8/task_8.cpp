50fd1da9e1f38f6166d496753be065beac3e1bd8
Folomeyev Andrey <andreyfoma092@gmail.com>
2025-12-08 20:15:11 +0300

    дз

---
+++ 2025.11.12 Homework-7/task_8/task_8.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdio>
+
+long long C(int n, int k)
+{
+    if (k == 0 || k == n)
+    {
+        return 1;
+    }
+    return C(n - 1, k - 1) + C(n - 1, k);
+}
+
+int main(int argc, char** argv)
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    printf("%lld\n", C(n, k));
+    return 0;
+}
