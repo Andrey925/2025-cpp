043d9e3ecbe84e6af9e7ef52bf580f708c3e01e5
Folomeyev Andrey <andreyfoma092@gmail.com>
2025-12-08 20:18:08 +0300

    дз

---
+++ 2025.11.05 Homework-5/task 1/task 1.cpp
@@ -0,0 +1,23 @@
+﻿#include<cstdio>
+#include<stdlib.h>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	scanf_s("%d", &n);
+	int *arr = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &arr[i]);
+	 }
+	for (int i = 0; i < n; i += 2)
+	{
+		printf("%d", arr[i]);
+		if (i + 2 < n)
+		{
+			printf(" ");
+		}
+	}
+	free(arr);
+	return 0;
+}
---
+++ 2025.11.05 Homework-5/task 6;/task 6;.cpp
@@ -0,0 +1,25 @@
+﻿#include<cstdio>
+#include<stdlib.h>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	scanf_s("%d", &n);
+	int count = 0;
+	int* arr = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &arr[i]);
+	}
+	for (int i = 1; i < n - 1; i++)
+	{
+		if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+		{
+			count++;
+		}
+		
+	}
+	printf("%d", count);
+	free(arr);
+	return 0;
+}
