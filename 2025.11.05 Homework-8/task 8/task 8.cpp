a523218721b76b5cf7d73b95cb2ac0cd2bf3ee7c
Folomeyev Andrey <andreyfoma092@gmail.com>
2025-12-08 20:23:32 +0300

    дз

---
+++ 2025.11.05 Homework-8/task 8/task 8.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    char s[1000];
+    scanf("%s", s);
+    int count[128] = { 0 }; 
+    for (int i = 0; s[i]; i++)
+        count[s[i]]++;
+    for (int i = 0; i < 128; i++)
+        if (count[i] == 2)
+        {
+            printf("%c\n", i);
+            break;
+        }
+    return 0;
+}
